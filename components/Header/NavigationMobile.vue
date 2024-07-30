49be025942766ade01213d490638b484b025bb48
minh <minhchiu.official@gmail.com>
2024-07-31 01:00:15 +0700

    feat(ui): add account header

--- components/Header/NavigationMobile.vue
+++ components/Header/NavigationMobile.vue
@@ -9,45 +9,57 @@ const menus = [
 
 const router = useRouter();
 const route = useRoute();
-const isOpen = ref(false);
+
+const íOpen = ref(false);
 
 const onClick = (href: string) => {
   router.push(href);
-  isOpen.value = false;
+  íOpen.value = false;
+};
+
+const handleUpdateOpen = (value: any) => {
+  íOpen.value = value;
 };
 </script>
 
 <template>
   <div class="lg:hidden">
-    <Sheet :open="isOpen">
+    <Sheet :open="íOpen" @update:open="handleUpdateOpen">
       <SheetTrigger as-child>
         <Button
           variant="outline"
           size="sm"
           class="border-none bg-white/10 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white focus:bg-white/30 focus-visible:ring-transparent focus-visible:ring-offset-0"
-          @click="isOpen = !isOpen"
         >
           <Icon name="lucide:menu" class="size-4" />
         </Button>
       </SheetTrigger>
 
       <SheetContent side="left" class="px-2">
+        <SheetHeader>
+          <SheetTitle>
+            <NuxtLink to="/">
+              <div class="flex items-center gap-2 px-3">
+                <img src="/logo-dark.svg" alt="logo" height="28" width="28" />
+                <p class="text-2xl font-semibold text-blue-600">Finance</p>
+              </div>
+            </NuxtLink></SheetTitle
+          >
+          <SheetDescription />
+        </SheetHeader>
+
         <nav class="flex flex-col gap-y-2 pt-6">
           <Button
             v-for="menu in menus"
             :key="menu.href"
             :variant="menu.href === route.path ? 'secondary' : 'ghost'"
             class="w-full justify-start"
+            :class="{ 'text-primary': menu.href === route.path }"
             @click="onClick(menu.href)"
           >
             {{ menu.label }}
           </Button>
         </nav>
-        <SheetFooter>
-          <SheetClose as-child>
-            <Button type="submit"> Save changes </Button>
-          </SheetClose>
-        </SheetFooter>
       </SheetContent>
     </Sheet>
   </div>
---
+++ pages/accounts.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const newAccount = reactive({
+  onOpen: () => {},
+});
+</script>
+
+<template>
+  <div class="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
+    <Card class="border-none drop-shadow-none">
+      <CardHeader
+        class="gap-y-2 lg:flex lg:flex-row lg:items-center lg:justify-between"
+      >
+        <CardTitle class="line-clamp-1 text-xl">Card Title</CardTitle>
+
+        <Button @click="newAccount.onOpen" size="sm">
+          <Icon name="carbon:add" class="mr-2 size-4" />
+          Add mew
+        </Button>
+      </CardHeader>
+    </Card>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
